d9da86b7ca9eddabb645313650686e101a02cc95
huiyinfan <huiyinfan@kuaishou.com>
2023-05-07 22:53:56 +0800

    feat: 组件库的坏境配置

---
+++ packages/b/index.js
@@ -0,0 +1,3 @@
+import sayHello from '@pg-fun/a';
+
+sayHello();
---
+++ play/app.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+      <ea-button>按钮</ea-button>
+      <Button>123</Button>
+    </div>
+  </template>
+  <script lang="ts" setup>
+  </script>
+  
