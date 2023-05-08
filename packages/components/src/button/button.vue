bf9d17ff2236ad5edea4654ce42ddb0dce458b73
huiyinfan <huiyinfan@kuaishou.com>
2023-05-08 20:52:37 +0800

    feat: 开发组件 & vite打包

---
+++ packages/components/src/button/button.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+import "./style/index.less";
+import {computed} from "vue";
+defineOptions({ name: 'pg-button' });
+
+type ButtonProps = {
+  type?: string;
+}
+
+const buttonProps = defineProps<ButtonProps>();
+const buttonStyle = computed(() => {
+  return {[`pg-button--${buttonProps.type}`]: buttonProps.type}
+});
+</script>
+<template>
+  <button class="pg-button" :class="buttonStyle">
+    <slot/>
+  </button>
+</template>
