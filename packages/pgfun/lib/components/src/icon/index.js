508bd9efe7f6876c9519ebd10d8f9dae8a50f101
huiyinfan <huiyinfan@kuaishou.com>
2023-05-11 16:59:21 +0800

    feat: release-it实现自动管理发布组件库

--- packages/pgfun/lib/components/src/icon/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperties(exports, { __esModule: { value: true }, [Symbol.toStringTag]: { value: "Module" } });
-const icon_vue_vue_type_script_setup_true_lang = require("./icon.vue.js");
-const index = require("../../../utils/withinstall/index.js");
-const Icon = index.default(icon_vue_vue_type_script_setup_true_lang.default);
-exports.Icon = Icon;
-exports.default = Icon;
